2c04be938c599b1e8862e6723d545a7b9ca567ba
Randall Leung <leungran@gmail.com>
2014-08-30 13:34:03 -0700

    form working, creating to database

--- app/controllers/unsaved_msgs_controller.rb
+++ app/controllers/unsaved_msgs_controller.rb
@@ -3,4 +3,20 @@ class UnsavedMsgsController < ApplicationController
 	def index
 	end
 
+	def new
+		@unsaved_msg = UnsavedMsg.new
+	end
+
+	def create
+		new_message = params[:unsaved_msg].permit(:email, :send_num, :content, :time)
+		message = UnsavedMsg.create(new_message)
+
+		
+
+		redirect_to '/'
+	end
+
+
+
+
 end
