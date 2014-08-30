b7bbc3ef07cb9ac670332b8bbf6ac3457af842be
Siejen Yin-Stevenson <siejenyin@gmail.com>
2014-08-30 15:52:44 -0700

    updated signup, login and logout Users CRUD

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,25 @@
+class SessionController < ApplicationController
+  def new
+    #my login form
+  end
+
+  def create
+    @user = User.authenticate(params[:user][:email], params[:user][:password])
+
+    if @user
+      session[:user_id] = @user.id
+      redirect_to root_path, :notice => "User Logged in Successfully!"
+    else
+      flash.now[:notice] = "Can't log you in"
+      render :new
+    end
+
+  end
+
+  def destroy
+      session[:user_id] = nil
+      # render text: "You've destroyed the session"
+      redirect_to '/'
+  end
+end
+
--- spec/controllers/unsaved_msgs_controller_spec.rb
+++ spec/controllers/unsaved_msgs_controller_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe UnsavedMsgsController, :type => :controller do
+# RSpec.describe UnsavedMsgsController, :type => :controller do
 
-end
+# end
