acaee22096c084563695fbe32ce733a18e25fcd3
Randall Leung <leungran@gmail.com>
2014-08-30 12:53:15 -0700

    password controller

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,2 +1,22 @@
-class PasswordsController < ApplicationController
-end
+# class PasswordsController < ApplicationController
+#  def new
+#   # Render reset password form
+#   end
+
+#   def create
+#   # render text: "About to reset password..."
+#   user = User.find_by_email(params[:email])
+
+#   if user
+#   #start password flow
+#   user.set_password_reset
+#   #password reset from user_mailer method
+#   UserMailer.password_reset(user).deliver
+#   end
+
+#   # render text: "User has been reset"
+#   # redirect_to "/"
+#   redirect_to login_url: "Email was sent with instructions"
+#   end
+
+# end
--- spec/models/unsaved_msg_spec.rb
+++ spec/models/unsaved_msg_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe UnsavedMsg, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "has necessary attributes" do
+    um = UnsavedMsg.new(email: "tacos@gmail.com", phone_num: "8008886666", subject: "Goodbye", content: "Today is Friday - best day of the week!", time: "8:00AM 2014-AUG-29", send_num: "6507044251")
+    expect(um.email).to eq("tacos@gmail.com")
+	expect(um.phone_num).to eq("8008886666")
+	expect(um.subject).to eq("Goodbye")
+	expect(um.content).to eq("Today is Friday - best day of the week!")
+	expect(um.time).to eq("8:00AM 2014-AUG-29")
+	expect(um.send_num).to eq("6507044251")
+  end
+
 end
