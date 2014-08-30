d661be89f0f082dd07fb1791851b288d66651ba2
Siejen Yin-Stevenson <siejenyin@gmail.com>
2014-08-30 14:24:29 -0700

    updates to passwords_controller_spec

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe SessionController, :type => :controller do
+# RSpec.describe SessionController, :type => :controller do
 
-end
+# end
