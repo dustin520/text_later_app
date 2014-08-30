b2addc9629b32a525a6dbcb277ee4889d80ca73b
SanaNasar <sanasehereen.cse@gmail.com>
2014-08-30 12:23:58 -0700

    Adding authentication in passwords controller

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,2 +1,18 @@
 class PasswordsController < ApplicationController
+	
+	def new
+  	# renders reset password form
+ 	end
+
+	def create
+  	user = User.find_by_email(params[:email])
+			if user
+  		 	# start password flow
+  		 	user.set_password_reset
+  		 	UserMailer.password_reset(user).deliver
+			end
+
+		# render text: "User has been reset"
+		redirect_to users_path, notice: "Email was sent with instructions"
+	end
 end
