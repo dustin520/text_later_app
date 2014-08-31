bf0a9a236d7a328ebba237dcc05c0154a2021a00
Dustin Yu <dustin520@gmail.com>
2014-08-30 18:00:02 -0700

    local merge

--- app/controllers/saved_msgs_controller.rb
+++ app/controllers/saved_msgs_controller.rb
@@ -16,7 +16,10 @@ class SavedMsgsController < ApplicationController
 		api_info
 
 		# binding.pry
+
 		
+		
+		@client = Twilio::REST::Client.new account_sid, auth_token
 		message = @client.account.messages.create(:body => new_msg[:content],
 		    :to => new_msg[:send_num],     # Replace with your phone number
 		    :from => "+14154888381")   # Replace with your Twilio number
@@ -75,7 +78,6 @@ class SavedMsgsController < ApplicationController
 		# Get your Account Sid and Auth Token from twilio.com/user/account
 		account_sid = ENV['TWILIO_SID']
 		auth_token = ENV['TWILIO_AUTH']
-		@client = Twilio::REST::Client.new account_sid, auth_token
 	end
 
 
--- spec/controllers/saved_msgs_controller_spec.rb
+++ spec/controllers/saved_msgs_controller_spec.rb
@@ -2,28 +2,20 @@ require 'rails_helper'
 
 RSpec.describe SavedMsgsController, :type => :controller do
 
-	
 
+	describe "GET #index" do
+		it "responds successfully, 200 status code" do 
+			get :index
+			expect(response).to be_success
+			expect(response).to have_http_status(200)
+		end
+		
+		it "renders the index template" do
+			get :index
+			expect(response).to render_template("index")
+		end
 
-
-  describe "GET #index" do
-    it "responds successfully with HTTP 200 status code" do
-      get :index
-      expect(response).to be_success
-      expect(response).to have_http_status(200)
-    end
-    it "renders the index template" do
-      get :index
-      expect(response).to render_template("index")
-    end
-    it "returns all of the posts" do
-      post1 = Post.create(:author => "Tim")
-      post2 = Post.create(:author => "Elie")
-      get :index
-      expect(assigns(:all_posts)).to match_array [post1, post2]
-    end
   end
-
 
 end
 
