fbf1ecdae51eb891498befbfa6e6958fd1c475f8
Randall Leung <leungran@gmail.com>
2014-08-30 17:39:17 -0700

    send to multiple users

--- app/controllers/unsaved_msgs_controller.rb
+++ app/controllers/unsaved_msgs_controller.rb
@@ -13,20 +13,24 @@ class UnsavedMsgsController < ApplicationController
 		# Storing messages to DB
 		new_message = params[:unsaved_msg].permit(:email, :send_num, :content, :time)
 		message = UnsavedMsg.create(new_message)
-		# binding.pry
 
 		# Keys for API
 		account_sid = ENV['TWILIO_SID']
 	  auth_token = ENV['TWILIO_AUTH']
+	  from = '+14154888381'
+
+	  # For Multiple Recipients
+	  to = new_message[:send_num].split(",")
 
 	  # Reaching out to Twilio API
-		@client = Twilio::REST::Client.new account_sid, auth_token
-		@client.account.messages.create(
-			:from => '+14154888381', 
-			:to => new_message[:send_num], 
-			:body => new_message[:content]
-		)
-
+	  to.each do |friends|
+			@client = Twilio::REST::Client.new account_sid, auth_token
+			@client.account.messages.create(
+				:from => from, 
+				:to => friends, 
+				:body => new_message[:content]
+			)
+		end
 
 		redirect_to '/'
 	end
