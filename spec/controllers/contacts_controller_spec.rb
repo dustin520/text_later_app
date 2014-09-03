3890694777243b7642ac6b552499f54d3aa42cdd
Dustin Yu <dustin520@gmail.com>
2014-09-02 18:53:33 -0700

    added and changed migrations for contacts and user; added contact model and controller

---
+++ spec/controllers/contacts_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe ContactsController, :type => :controller do
+
+  describe "GET index" do
+    it "returns http success" do
+      get :index
+      expect(response).to be_success
+    end
+  end
+
+  describe "GET new" do
+    it "returns http success" do
+      get :new
+      expect(response).to be_success
+    end
+  end
+
+  describe "GET edit" do
+    it "returns http success" do
+      get :edit
+      expect(response).to be_success
+    end
+  end
+
+end
