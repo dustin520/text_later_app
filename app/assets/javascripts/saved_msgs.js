28d4cc472b32376fcc7ba49bd7938322395ca255
Dustin Yu <dustin520@gmail.com>
2014-09-03 01:37:43 -0700

    added styling and connected template partial

--- app/assets/javascripts/saved_msgs.js
+++ app/assets/javascripts/saved_msgs.js
@@ -1,4 +1,35 @@
+
 $(document).ready(function(){
-	
+
+	$(".yobutton").on("click", function(){
+		$(".content-body").val("Don't forget to come to Cameron's Potluck!  Bring your food!");
+		$(".subject-body").val("WDI Potluck");
+	});
+
+	$(".birthdaybutton").on("click", function(){
+		$(".content-body").val("Happy Birthday!");
+		$(".subject-body").val("Birthday");
+	});
+
+	$(".xmasbutton").on("click", function(){
+		$(".subject-body").val("Christmas");
+		$(".content-body").val("Merry Christmas!");
+	});
+
+	$(".reminderbutton").on("click", function(){
+		$(".content-body").val("Don't Forget!");
+		$(".subject-body").val("Reminder");
+	});
+
+	$(".thinkbutton").on("click", function(){
+		$(".content-body").val("Thinking of you right now.");
+		$(".subject-body").val(L"ifelong Partner");
+	});
+
+	$(".dogbutton").on("click", function(){
+		$(".content-body").val("Walk the dog.");
+		$(".subject-body").val("Dog Activity");
+	});
+
 
 });
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,4 @@ class ApplicationController < ActionController::Base
   	@current_user ||= User.find_by(id: session[:user_id])
   end
 
-  
-
 end
