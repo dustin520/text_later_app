10984f0db4f946087b479e2b95ffc76d440c909b
Dustin Yu <dustin520@gmail.com>
2014-09-04 19:16:32 -0700

    heroku config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,3 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
+
+url = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/")
+REDIS = Redis.new(url: url)
