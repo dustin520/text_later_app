88e248c122f54a3ca1f5cf2b09b26247dd9fc7c3
Siejen Yin-Stevenson <siejenyin@gmail.com>
2014-09-07 22:50:15 -0700

    updated additional rspec files - 10 passing tests

--- spec/controllers/saved_msgs_controller_spec.rb
+++ spec/controllers/saved_msgs_controller_spec.rb
@@ -2,21 +2,20 @@ require 'rails_helper'
 
 RSpec.describe SavedMsgsController, :type => :controller do
 
-
 	describe "GET #index" do
 		it "responds successfully, 200 status code" do 
-			get :index
+    		get :index, :user_id => 1
 			expect(response).to be_success
 			expect(response).to have_http_status(200)
 		end
 		
 		it "renders the index template" do
-			get :index
-			expect(response).to render_template("index")
+			route_params = {:user_id => 1}
+			session_params = {:user_id => 1}
+	   		get :index, route_params, session_params
+			expect(response).to render_template(:index)
 		end
 
   end
 
 end
-
-
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Contact, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	it "has necessary attributes" do
+		c = Contact.new(first_name: "Jerry", last_name: "Wong", phone_num: "23478900983")
+		expect(c.first_name).to eq("Jerry")
+		expect(c.last_name).to eq("Wong")
+		expect(c.phone_num).to eq("23478900983")				
+	end
+
 end
